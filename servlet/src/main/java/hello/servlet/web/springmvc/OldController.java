651cd9d2b1b9aa2d38ba7088e5cc18398b1524c6
all-day-and-night <jwug0821@gmail.com>
2022-01-03 16:21:01 +0900

    old spring controller

--- servlet/src/main/java/hello/servlet/web/springmvc/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/OldController.java
@@ -8,17 +8,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-
 
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
-
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("OldController.handleRequest");
         return null;
     }
-
-
 }
